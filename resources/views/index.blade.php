1ebda77c1b229a6c39968f23c01b9b2d9b6c8297
Tarun Krishnan <tarun@tarunkrishnan.com>
2015-05-14 21:03:42 -0500

    initial addition of full-screen video

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,24 @@
 @extends('layouts.master')
+	
+@section('content')
+	
+	<div class="matte" style="display:block;width:100%;height:100%;min-height:100%; min-width:100%; background:rgba(0, 0, 0, .6);z-index:9999;"></div>
+	
+@stop
+
+@section('scripts')
+<script type="text/javascript" src="{{ URL::asset('js/okvideo/src/okvideo.js') }}"></script>
+<script>
+	$('document').ready(function(){
+		$(function(){
+          $.okvideo({ source: 'https://vimeo.com/102795504', volume : 0 }) 
+        });
+       
+       $('.matte').css({
+	     width : $(window).width(),
+	     height : $(window).height()  
+       });
+	});
+</script>
+	
+@stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,7 +30,7 @@
     @include('layouts.pages.navigation')
     
     
-    
+    @yield('content')
 
 
     <!-- Bootstrap core JavaScript
@@ -38,5 +38,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+    @yield('scripts')
   </body>
 </html>
