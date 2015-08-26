080a42a321dac486f154d18a6c83334a28eb7392
Tarun <tarun@colossal.net>
2015-08-26 11:46:26 -0500

    minor updates

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="container home-video" style="position:relative;">
 		<div class="jumbotron" id="homepage" style="padding:0;positional:relative; background:url({{ URL::asset('img/video-bg.jpg') }}); background-size:cover;overflow:hidden;">
-			<img src="{{ URL::asset('img/video-overlay.png') }}" style="display:block;width:300px;height:83px;position:absolute;z-index:100;top: 50%;
+			<img class="overlay" src="{{ URL::asset('img/video-overlay.png') }}" style="display:block;width:300px;height:83px;position:absolute;z-index:100;top: 50%;
    left: 50%;margin-left:-150px; margin-top:0px;" alt="" />
    <div class="matte" style="position:absolute;width:100%; height:100%; display:block;z-index:101;"></div>
 			<iframe src="https://player.vimeo.com/video/102795504?autoplay=1&loop=1&color=66C3AE&title=0&byline=0&portrait=0" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
@@ -24,6 +24,9 @@
 			setTimeout(function(){
 				$('#homepage iframe').animate({opacity: 1}, 4000);
 			}, 1000);
+			setTimeout(function(){
+				$('img.overlay').animate({opacity: 0}, 4000)
+			}, 4000);
 		});
 	</script>
 @stop
