42a1d6f2f4e96d79b8f79381d6020a5bcf95e8e9
Tarun <tarun@colossal.net>
2015-08-26 09:28:12 -0500

    added overlay

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,13 @@
 	
 @section('content')
 
-	<div class="container home-video">
-		<div class="jumbotron" id="homepage" style="padding:0;positional:relative; background:url({{ URL::asset('img/video-bg.jpg') }}); background-size:cover;">
+	<div class="container home-video" style="position:relative;">
+		<div class="jumbotron" id="homepage" style="padding:0;positional:relative; background:url({{ URL::asset('img/video-bg.jpg') }}); background-size:cover;overflow:hidden;">
+			<img src="{{ URL::asset('img/video-overlay.png') }}" style="display:block;width:300px;height:83px;position:absolute;z-index:100;top: 50%;
+   left: 50%;margin-left:-150px; margin-top:0px;" alt="" />
+   <div class="matte" style="position:absolute;width:100%; height:100%; display:block;z-index:101;"></div>
 			<iframe src="https://player.vimeo.com/video/102795504?autoplay=1&loop=1&color=66C3AE&title=0&byline=0&portrait=0" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+			
 		</div>
 	</div>
 
