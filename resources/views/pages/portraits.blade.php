6c9521f36d340b119e490c0a5c753dd6d3b4c870
Tarun <tarun@colossal.net>
2015-08-26 11:08:58 -0500

    minor updates

--- resources/views/pages/portraits.blade.php
+++ resources/views/pages/portraits.blade.php
@@ -1,7 +1,64 @@
 @extends('layouts.master')
 	
 @section('content')
-
+	
+	<div class="container page-banner">
+		<h1 class="page-header">Portraits</h1>
+		<div class="jumbotron" style="background:url({{ URL::asset('img/page-banner-portraits.jpg') }}) top center;">
+			<h2>Capture those moments when you are in your element</h2>
+		</div>
+		<br /><br />
+	</div>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>	
+		</div>
+	</div>
 	
 @stop
 
--- resources/views/pages/weddings.blade.php
+++ resources/views/pages/weddings.blade.php
@@ -1,7 +1,71 @@
 @extends('layouts.master')
 	
 @section('content')
-
+	
+	<div class="container page-banner">
+		<div class="row">
+			<div class="col-md-9">
+				<h1 class="page-header">Weddings</h1>
+				<div class="jumbotron" style="background:#eee top center;">
+					<h2>Witness the embodiment of awe, wonder, and pure joy</h2>
+				</div>
+				<br /><br />
+			</div>
+			<div class="col-md-3">
+				
+			</div>
+		</div>
+	</div>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="thumbnail">
+					<img src="http://placehold.it/500" alt="" width="100%" />
+				</div>
+			</div>	
+		</div>
+	</div>
 	
 @stop
 
